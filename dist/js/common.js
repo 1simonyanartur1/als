d965702c1360589384668e0f31ed35f827e100fc
Artur <1simonyanartur1@gmail.com>
2020-08-10 07:45:46 +0300

    (feat) add mobile

--- dist/js/common.js
+++ dist/js/common.js
@@ -47,22 +47,49 @@
 			nextArrow: '<button class="icon-arrow2 right"></button>',
 			slidesToScroll: 1,
 			infinite: false,
-			rows: 3
+			rows: 3,
+			responsive: [
+				{
+					breakpoint: 700,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1
+					}
+				}
+			]
+		});
+
+		$(document).on('click', '.burger', function () {
+			if (!$(this).hasClass('active')) {
+				$('.header .menu').slideDown();
+				$(this).addClass('active');
+			} else {
+				$('.header .menu').slideUp();
+				$(this).removeClass('active');
+			}
+		});
+
+		$(document).on('click', '.anchor', function (e) {
+			var fixed_offset = 50;
+			$('html, body').stop().animate({
+				scrollTop: $(this.hash).offset().top - fixed_offset
+			}, 500);
+			e.preventDefault();
 		});
 
 		$('.year-list').hide();
-		$(document).on('click', '.year__title', function(e) {
+		$(document).on('click', '.year__title', function (e) {
 			$(this).toggleClass('active');
 			$(this).next('.year-list').slideToggle();
 		});
-		$(document).on('click', '.year-list li', function(e) {
+		$(document).on('click', '.year-list li', function (e) {
 			var year = $(this).data('year');
 			$(this).parent('.year-list').slideToggle();
 			$('.year__title span').text(year);
 			$('.year__title').toggleClass('active');
 
-			$('.news .inner').each(function(e) {
-				if($(this).data('year') == year) {
+			$('.news .inner').each(function (e) {
+				if ($(this).data('year') == year) {
 					$('.news .inner').hide();
 					$(this).show();
 				}
@@ -70,9 +97,23 @@
 		});
 
 		$('.spoiler-list').hide();
-		$(document).on('click', '.spoiler__title', function(e) {
+		$(document).on('click', '.spoiler__title', function (e) {
 			$(this).toggleClass('active');
 			$(this).next('.spoiler-list').slideToggle();
+		});
+
+		$(document).on('click', '.information-menu a', function (e) {
+			e.preventDefault();
+			var tab = $(this).data('id');
+			$('.information-menu a').removeClass('active');
+			$(this).addClass('active');
+
+			$('.information .text-container').each(function (e) {
+				if ($(this).data('id') == tab) {
+					$('.information .text-container').hide();
+					$(this).show();
+				}
+			});
 		});
 
 	});
